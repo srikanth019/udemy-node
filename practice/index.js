93e0d1d1800d9d86b7d0dd0d04cd64ac26e24c88
srikanth019 <srikanth.golla@brainvire.com>
2023-01-21 18:09:03 +0530

    notes upto serve files

---
+++ practice/index.js
@@ -0,0 +1,24 @@
+function x(){
+    let ab= "an";
+    function k(){
+        
+        if (true){
+            const am="sjvdjdfah"
+            if(true){
+                var nm="bvbcdjhfja"
+                
+            }
+            console.log(nm);
+        }
+            
+       
+    }
+    
+    k();
+   
+    
+    
+}
+var a="hchc";
+x();
+
