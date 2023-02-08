c14a13a53e6952034497392f095a0b25939cbaab
srikanth019 <srikanth.golla@brainvire.com>
2023-02-08 16:39:45 +0530

    upto sequelize

--- online/app.js
+++ online/app.js
@@ -8,6 +8,10 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -39,6 +43,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
+
 
 sequelize
 // .sync({force: true})
@@ -55,6 +67,9 @@ sequelize
 })
 .then(user => {
     //console.log(user);
+    return user.createCart();
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
