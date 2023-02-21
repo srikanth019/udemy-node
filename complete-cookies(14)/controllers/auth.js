3702910afb502bfa391d79c4cde03c1e85003c9d
srikanth019 <srikanth.golla@brainvire.com>
2023-02-21 15:46:19 +0530

    complete cookies and sessions

---
+++ complete-cookies(14)/controllers/auth.js
@@ -0,0 +1,35 @@
+const User = require('../models/user')
+
+exports.getLogin = (req, res, next) => {
+    // isLogedIn = req.get('Cookie').split('=')[1] === 'true';
+    // console.log(req.session.isLogedIn);
+    res.render('auth/login', {
+        pageTitle: 'Login',
+        path: '/login',
+        isAuthenticated: false
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    // res.setHeader('Set-Cookie','logedIn=true')
+    User.findById("63ecd33a9d5a76ffb3007983")
+    .then(user => {
+        req.session.isLogedIn = true;
+        req.session.user = user;
+        req.session.save((err) => {
+            console.log(err);
+            res.redirect('/');
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err)=> {
+        console.log(err);
+        res.redirect('/');
+    });
+};
